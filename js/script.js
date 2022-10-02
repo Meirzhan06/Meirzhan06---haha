1f41d648d58dd93978a0bf08c0a09c1d427e7769
Anonim <103999938+Meirzhan06@users.noreply.github.com>
2022-10-02 22:21:01 +0600

    fewfwef

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(window).on('load', function() {
     $preloader = $('.preloader'),
     $loader = $preloader.find('.preloader');
     $loader.fadeOut();
-    $preloader.delay(1500).fadeOut('slow');
+    $preloader.delay(0).fadeOut('slow');
   });
   
 $(document).ready(function(){
@@ -11,3 +11,52 @@ $(document).ready(function(){
         $('body').toggleClass('lock');
     })
 });
+$(document).ready(function(){
+    $('.slider').slick({
+        dots:true,
+        adaptiveHeight:true,
+        arrows:false,
+        slidesToShow:3,
+        infinite: true,
+        
+    });
+});
+$('.slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    infinite: true,
+    arrows:false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          infinite: true,
+          arrows:false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          arrows:false,
+          infinite: true,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          arrows:false,
+          infinite: true,
+          slidesToScroll: 1
+        }
+      }
+
+    ]
+  });
---
+++ telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/bot5612211712:AAGyZ9865H21ywdKovZ-1Oq-u89BGuJQnaA/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "5612211712:AAGyZ9865H21ywdKovZ-1Oq-u89BGuJQnaA";
+$chat_id = "-866006724";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: index.html');
+} else {
+  echo "Error";
+}
+?>
